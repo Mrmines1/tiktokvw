4c5737c77dd7be6a0524dcb1e07526d13ca4a9cf
Mrmines1 <117607642+Mrmines1@users.noreply.github.com>
2023-01-21 21:07:53 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='TikTokAutomatedViewbot',
+    version='1.0',
+    packages=['TikTokAutomatedViewbot'],
+    url='https://github.com/user/TikTokAutomatedViewbot',
+    license='MIT',
+    description='A script that automates views on TikTok videos using Selenium and Firefox.',
+    install_requires=[
+        'selenium',
+        'geckodriver-autoinstaller',
+    ],
